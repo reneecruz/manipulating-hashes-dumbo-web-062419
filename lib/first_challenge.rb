63022974430b96ac8e6450b17b6ce8ccdb1f9425
Renée Cruz <reneebasscruz@gmail.com>
2019-06-20 02:15:39 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -17,7 +17,7 @@ def first_challenge
   #your code here
   contacts["Freddy Mercury"][:favorite_icecream_flavors].each_with_index do |index, flavor|
       if flavor == "strawberry"
-         contacts["Freddy Mercury"][:favorite_icecream_flavors].delete
+         contacts["Freddy Mercury"][:favorite_icecream_flavors].delete_at(index)
       end
   end
   
