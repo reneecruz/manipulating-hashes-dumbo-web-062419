26a88aa90107407f831c9fe6dcce4f8a5222309a
Renée Cruz <reneebasscruz@gmail.com>
2019-06-20 01:22:27 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -22,7 +22,7 @@ def first_challenge
         value.each do |flavor|
         #array.delete_if {|flavor| ==  "strawberry" }  
           #contacts["Freddy Mercury"][:favorite_icecream_flavor].delete_if {|attribute, value| value >= "strawberry" } 
-          contacts.delete_if(:favorite_icecream_flavor ["strawberry"])
+          #contacts.delete_if(:favorite_icecream_flavor ["strawberry"])
           e.
           favorite_icecream_flavor.delete_if{|key, value|key == "strawberry"}
           puts "#{flavor}"
